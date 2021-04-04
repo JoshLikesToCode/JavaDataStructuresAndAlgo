af22cd3911b4cd0b44a3fde96f0b849f416cc4a3
Joshua <jstone925314@gmail.com>
2021-04-04 12:00:32 -0700

    Programming Projects 3.1-3.6

--- src/ArrayBub.java
+++ src/ArrayBub.java
@@ -43,6 +43,13 @@ public class ArrayBub {
         System.out.println("");
     }
 
+    private void swap(int one, int two)
+    {
+        long temp = a[one];
+        a[one] = a[two];
+        a[two] = temp;
+    }
+
     public void bubbleSort()
     {
         int out, in;
@@ -59,11 +66,79 @@ public class ArrayBub {
         }
     }
 
-    private void swap(int one, int two)
+    /** Exercise 3.1:
+    In the bubblesort() the in index always goes from left to right, finding the largest item and carrying
+    it toward out on the right. Modify the bubbleSort() method so that it's bidirectional. This means the in
+    index will first carry the largest item from left to right as before, but when it reaches out, it will
+    reverse and carry the smallest item from right to left. You'll need two outer indexes, one on the right
+            (the old out) and another on the left. */
+    public void exercise_bubbleSort()
     {
-        long temp = a[one];
-        a[one] = a[two];
-        a[two] = temp;
+        int out = nElems - 1, in, left_out = 0;
+        boolean swap = true;
+
+        while(swap) {
+            swap = false;
+
+            for(in = left_out; in < out; in++)
+            {
+                if(a[in] > a[in+1])
+                {
+                    swap(in, in+1);
+                    swap = true;
+                }
+            }
+
+            for(in = out-1; in > left_out; in--)
+            {
+                if(a[in] < a[in-1])
+                {
+                    swap(in, in-1);
+                    swap = true;
+                }
+            }
+
+            left_out++;
+            out--;
+        }
+    }
+
+    /**
+     *         Exercise 3.4:
+     *         Another simple sort is the odd-even sort. The idea is to repeatedly make two passes through the array. On the
+     *         first pass you look at all the pairs of items, a[j] and a[j+1], where j is odd (j = 1, 3, 5...). If their key
+     *         values are out of order, you swap them. One the second pass you do the same for all the even values (j = 2, 4, 6...).
+     *         You do these two passes repeatedly until the array is sorted. Replace the bubbleSort() method with an oddEvenSort()
+     *         method. Make sure it works for varying amounts of data. You'll need to figure out how many times to do the two passes.
+     *         The odd-even sort is actually useful in a multiprocessor environment, where a separate processor can operate on each
+     *         odd pair simultaneously and then on each even pair. Because the odd pairs are independent of each other, each pair
+     *         can be checked - and swapped, if necessary - by a different processor. This makes for a very fast sort.
+     */
+    public void odd_even_sort() {
+        boolean swap = true;
+
+        while(swap)
+        {
+            swap = false;
+
+            for(int in = 1; in < nElems - 1; in += 2)
+            {
+                if(a[in] > a[in+1])
+                {
+                    swap(in, in+1);
+                    swap = true;
+                }
+            }
+
+            for(int in = 0; in < nElems - 1; in += 2)
+            {
+                if(a[in] > a[in+1])
+                {
+                    swap(in, in+1);
+                    swap = true;
+                }
+            }
+        }
     }
 }
 
@@ -86,7 +161,8 @@ class BubbleSortApp{
         arr.insert(33);
         arr.display();
 
-        arr.bubbleSort();
+        //arr.bubbleSort();
+        arr.odd_even_sort();
         arr.display();
     }
 }
--- src/ArrayIns.java
+++ src/ArrayIns.java
@@ -71,6 +71,84 @@ public class ArrayIns {
     }
 
     /**
+     *     Exercise 3.5:
+     *         Modify the insertionSort() method so it counts the number of copies and the number of comparisons it makes during
+     *         a sort and displays the totals. To count the comparisons, you'll need to break up the double condition in the inner
+     *         while loop. Use this program to measure the number of copies and comparisons for different amounts of inversely
+     *         sorted data. Do the results verify O(N^2) efficiency? Do the same for almost-sorted dat (only a few items out
+     *         of place.) What can you deduce about the efficiency of this algorithm for almost-sorted data?
+     */
+    public void modified_insertionSort()
+    {
+        int in, out;
+        int num_of_copies = 0, num_of_comp = 0;
+
+        for(out = 1; out < nElems; out++)       // out is the dividing line
+        {
+            long temp = a[out];                 // remove marked item
+            in = out;                           // start shifts at out
+            while(in > 0 && a[in-1] >= temp)    // until one is smaller,
+            {
+                a[in] = a[in - 1];                // shift item to right
+                num_of_copies++;
+                num_of_comp++;
+                --in;                           // go left one position
+            }
+            num_of_comp++;
+            a[in] = temp;                       // insert marked item
+            num_of_copies++;
+        }
+        System.out.println("Input = " + this.nElems);
+        System.out.println("Number of copies = " + num_of_copies);
+        System.out.println("Number of comparisions = " + num_of_comp);
+    }
+
+    /**
+     * Exercise 3.6:
+     *         Here's an interesting way to remove duplicates from an array. The insertion soert uses a loop-within-a-loop algorithm
+     *         that compares every item in the array with every other item. If you want to remove duplicates, this is one way to start.
+     *         Modify the insertionSort() method so that it removes duplicates as it sorts. Here's one approach: When a duplicate is
+     *         found, write over one of the duplicates items with a key value less than any normally used (such as -1, if all the normal
+     *         keys are positive). Then the normal insertion sort algorithm, treating this new key like any other item, will put it at
+     *         index 0. From now on the algorithm can ignore this item. The next duplicate will go at index 1, and so on. When the sort
+     *         is finished, all the removed dupes (now represented by -1 values) will be found at the beginning of the array. The array
+     *         can then be resized and shifted down so it starts at 0.
+     */
+    public void noDups_insertionSort()
+    {
+        int in, out;
+        int noDups = 0;
+
+        for(out = 1; out < nElems; out++)
+        {
+            long temp = a[out];
+            in = out;
+
+            while(in > 0 && a[in-1] >= temp)
+            {
+                if(a[in-1] == temp)
+                {
+                    temp = -1;
+                    noDups++;
+                }
+                a[in] = a[in-1];
+                --in;
+            }
+            a[in] = temp;
+        }
+
+        /* removed marked dupes */
+        if(noDups != 0)
+        {
+            for(int i = noDups; i < nElems; i++)
+            {
+                a[i - noDups] = a[i];
+            }
+        }
+        nElems -= noDups;
+    }
+
+    /**
      * Exercise 3.2 : median() function
      * @return the median value of the array
      */
@@ -85,7 +163,7 @@ public class ArrayIns {
     }
 
     /** Helper function for Exercise 3.3: noDups() below
-     * @param index index for element that needs to be deleted from array
+     * @param index for element that needs to be deleted from array
      */
     public void delete(int index)
     {
@@ -107,6 +185,7 @@ public class ArrayIns {
                 if(a[j] == a[i])
                 {
                     this.delete(j);
+                    nElems--;
                 }
             }
         }
@@ -136,12 +215,15 @@ class InsertSortApp
         arr.insert(12);
         arr.insert(12);
 
+        arr.noDups_insertionSort();
         arr.display();
+
         System.out.println("Array median = " + arr.median());
+        //arr.noDups();
+        //arr.display();
 
         arr.insertionSort();
-        arr.display();
-        arr.noDups();
+        arr.modified_insertionSort();
         arr.display();
     }
 }
